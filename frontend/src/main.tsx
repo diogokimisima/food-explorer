5f96026803b4783a8c63c4e51c71ee18d9a6ce63
dkimisima@gmail.com <dkimisima@gmail.com>
2024-10-14 22:48:14 -0300

    update verce.json

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,41 +1,41 @@
 // main.tsx ou index.tsx
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
-import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import { App } from "./App";
+import { createBrowserRouter, RouterProvider, Navigate } from "react-router-dom";
+import { App } from "./App"; // Importando o App
 import { SignIn } from "./components/sign-in";
 import { SignUp } from "./components/sign-up";
 import { Home } from "./components/home";
 import { NewProduct } from "./components/new-product";
 import { ShowProduct } from "./components/show-product";
 import { UpdateProduct } from "./components/update-product";
-import { Order } from "./components/order";
 import './global.css';
+import { Order } from "./components/order";
 
 const isAuthenticated = true;
 
+const routes = createBrowserRouter([
+  {
+    path: "/", 
+    element: <App />,
+    children: isAuthenticated
+      ? [
+          { path: "/", element: <Home /> },
+          { path: "new-product", element: <NewProduct /> },
+          { path: "show-product", element: <ShowProduct /> },
+          { path: "update-product", element: <UpdateProduct /> },
+          { path: "order", element: <Order /> },
+          { path: "*", element: <Navigate to="/" replace /> },
+        ]
+      : [
+          { path: "signup", element: <SignUp /> },
+          { path: "*", element: <SignIn /> }, 
+        ],
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<App />}>
-          {isAuthenticated ? (
-            <>
-              <Route index element={<Home />} />
-              <Route path="new-product" element={<NewProduct />} />
-              <Route path="show-product" element={<ShowProduct />} />
-              <Route path="update-product" element={<UpdateProduct />} />
-              <Route path="order" element={<Order />} />
-              <Route path="*" element={<Navigate to="/" replace />} />
-            </>
-          ) : (
-            <>
-              <Route path="signup" element={<SignUp />} />
-              <Route path="*" element={<SignIn />} />
-            </>
-          )}
-        </Route>
-      </Routes>
-    </Router>
+    <RouterProvider router={routes} />
   </React.StrictMode>
 );
